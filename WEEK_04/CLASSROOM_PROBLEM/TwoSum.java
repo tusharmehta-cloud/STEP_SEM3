a4025b517c39bba701fc648fd9d305f3ad90530b
tusharmehta-cloud <tusharmehta31jan@gmail.com>
2026-09-19 01:40:33 +0530

    Add Two Sum classroom problem

---
+++ WEEK_04/CLASSROOM_PROBLEM/TwoSum.java
@@ -0,0 +1,48 @@
+package WEEK_04.CLASSROOM_PROBLEM;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class TwoSum {
+
+    static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("Indices: " + Arrays.toString(result));
+
+        sc.close();
+    }
+}
