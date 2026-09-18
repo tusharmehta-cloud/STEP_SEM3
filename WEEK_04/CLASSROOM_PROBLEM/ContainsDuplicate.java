03c866e27d884af647847b4b0a0af51e0022b3a3
tusharmehta-cloud <tusharmehta31jan@gmail.com>
2026-09-19 01:43:42 +0530

    Add Contains Duplicate classroom problem

---
+++ WEEK_04/CLASSROOM_PROBLEM/ContainsDuplicate.java
@@ -0,0 +1,43 @@
+package WEEK_04.CLASSROOM_PROBLEM;
+
+import java.util.Scanner;
+
+public class ContainsDuplicate {
+
+    static boolean containsDuplicate(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        boolean result = containsDuplicate(nums);
+
+        System.out.println("Contains Duplicate: " + result);
+
+        sc.close();
+    }
+}
