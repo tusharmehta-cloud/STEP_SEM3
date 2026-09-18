aa69328775926cde2bd4d5ef6f910e6501d98405
tusharmehta-cloud <tusharmehta31jan@gmail.com>
2026-09-19 01:42:37 +0530

    Add Best Time to Buy and Sell Stock classroom problem

---
+++ WEEK_04/CLASSROOM_PROBLEM/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,49 @@
+package WEEK_04.CLASSROOM_PROBLEM;
+
+import java.util.Scanner;
+
+public class BestTimeToBuyAndSellStock {
+
+    static int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter stock prices:");
+
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int result = maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + result);
+
+        sc.close();
+    }
+}
