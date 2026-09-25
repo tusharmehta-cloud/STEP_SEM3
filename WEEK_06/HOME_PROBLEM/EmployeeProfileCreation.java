7e72807b6faad1300e562467dae4d43723b4d138
tusharmehta-cloud <tusharmehta31jan@gmail.com>
2026-09-25 22:41:47 +0530

    Add Employee Profile Creation home problem

---
+++ WEEK_06/HOME_PROBLEM/EmployeeProfileCreation.java
@@ -0,0 +1,60 @@
+package WEEK_06.HOME_PROBLEM;
+
+public class EmployeeProfileCreation {
+
+    static class Employee {
+
+        String empId;
+        String empName;
+        double salary;
+        boolean isIntern;
+
+        public Employee(
+                String empId,
+                String empName,
+                double salary) {
+
+            this.empId = empId;
+            this.empName = empName;
+            this.salary = salary;
+            this.isIntern = false;
+        }
+
+        public Employee(
+                String empId,
+                String empName) {
+
+            this(empId, empName, 0);
+            this.isIntern = true;
+        }
+
+        void printProfile() {
+
+            System.out.println(
+                    empId + " | "
+                    + empName + " | Rs "
+                    + salary + " | Intern: "
+                    + isIntern
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee permanentEmployee =
+                new Employee(
+                        "E-101",
+                        "Divya",
+                        65000
+                );
+
+        Employee internEmployee =
+                new Employee(
+                        "E-102",
+                        "Arjun"
+                );
+
+        permanentEmployee.printProfile();
+        internEmployee.printProfile();
+    }
+}
